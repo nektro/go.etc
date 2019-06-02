b7dac52f25a405e860209344d67f85d500c11530
Meghan <nektromc@gmail.com>
2019-06-02 04:56:50 -0400

    Create config.go

---
+++ config.go
@@ -0,0 +1,24 @@
+package etc
+
+import (
+	"encoding/json"
+	"reflect"
+
+	. "github.com/nektro/go-util/alias"
+	. "github.com/nektro/go-util/util"
+)
+
+func InitConfig(path string, template interface{}) {
+	DieOnError(Assert(DoesFileExist(path), "config.json does not exist!"))
+	json.Unmarshal(ReadFile(path), &template)
+}
+
+func ConfigAssertKeysNonEmpty(config interface{}, keys ...string) {
+	v := reflect.ValueOf(config).Elem().Elem()
+	for _, k := range keys {
+		f := v.FieldByName(k).String()
+		t, _ := v.Type().FieldByName(k)
+		g := t.Tag.Get("json")
+		DieOnError(Assert(f != "", F("config[%s] is empty!", g)))
+	}
+}
