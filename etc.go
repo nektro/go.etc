c4f11a7fd5e10a76569f1ffe80ed0b24c3ac6990
Meghan Denny <nektromc@gmail.com>
2020-06-26 14:46:42 -0400

    move jwt functions out to own file

--- etc.go
+++ etc.go
@@ -224,28 +224,3 @@ func FixBareVersion() {
 	}
 	Version += "-" + runtime.Version()
 }
-
-// JWTSet sets a 'jwt' cookie on the provided ResponseWriter
-func JWTSet(w http.ResponseWriter, sub string) {
-	n, _ := os.Hostname()
-	http.SetCookie(w, &http.Cookie{
-		Name:   "jwt",
-		Value:  jwt.Get("astheno."+AppID+"."+Version+"."+n, sub, JWTSecret, Epoch, time.Hour*24*30),
-		MaxAge: 0,
-	})
-}
-
-// JWTGetClaims reads the 'jwt' cookie and returns claims within it, if they are valid
-func JWTGetClaims(c *htp.Controller, r *http.Request) jwt.MapClaims {
-	clms, err := jwt.VerifyRequest(r, JWTSecret)
-	c.Assert(err == nil, "403: "+err.Error())
-	return clms
-}
-
-// JWTDestroy tells the ResponseWriter to delete the 'jwt' cookie
-func JWTDestroy(w http.ResponseWriter) {
-	http.SetCookie(w, &http.Cookie{
-		Name:   "jwt",
-		MaxAge: -1,
-	})
-}
