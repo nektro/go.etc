d1a90c61ce13be730f6199433bd23b441d2e4973
Meghan Denny <nektromc@gmail.com>
2020-06-27 21:05:35 -0400

    htp: add convenience getters to controller

--- htp/controller.go
+++ htp/controller.go
@@ -2,6 +2,7 @@ package htp
 
 import (
 	"net/http"
+	"strconv"
 )
 
 // Controller is the htp package's extension
@@ -24,4 +25,23 @@ func (v *Controller) Assert(condition bool, message string) {
 // RedirectIf will redirct to location if condition is met
 func (v *Controller) RedirectIf(condition bool, location string) {
 	v.panic("redirect", !condition, location)
+}
+
+func (v *Controller) GetQueryString(name string) string {
+	s := v.r.URL.Query().Get(name)
+	v.Assert(len(s) > 0, "400: missing query value: "+name)
+	return s
+}
+
+func (v *Controller) GetFormString(name string) string {
+	s := v.r.Form.Get(name)
+	v.Assert(len(s) > 0, "400: missing form value: "+name)
+	return s
+}
+
+func (v *Controller) GetFormInt(name string) (string, int64) {
+	s := v.GetFormString(name)
+	n, err := strconv.ParseInt(s, 10, 64)
+	v.Assert(err == nil, "400: form value must be a number: "+name)
+	return s, n
 }
