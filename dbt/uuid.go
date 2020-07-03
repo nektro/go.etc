593f51f22cfa2aa4f62fbbdec733ab900b78f15a
Meghan Denny <nektromc@gmail.com>
2020-07-03 19:12:52 -0400

    dbt: add uuid type

---
+++ dbt/uuid.go
@@ -0,0 +1,57 @@
+package dbt
+
+import (
+	"database/sql/driver"
+	"errors"
+	"math/rand"
+	"time"
+
+	"github.com/nektro/go.etc/internal"
+	"github.com/oklog/ulid"
+)
+
+// UUID is a custom sql/driver type to handle list columns
+type UUID string
+
+// NewUUID creates a new UUID
+func NewUUID() string {
+	t := time.Unix(0, time.Now().UnixNano()-internal.Epoch.UnixNano())
+	var entropy = ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
+	return ulid.MustNew(ulid.Timestamp(t), entropy).String()
+}
+
+// Scan implements the database/sql/driver Scanner interface
+func (t *UUID) Scan(value interface{}) error {
+	if value == nil {
+		*t = ""
+		return nil
+	}
+	if bv, err := driver.String.ConvertValue(value); err == nil {
+		if v, ok := bv.(string); ok {
+			if len(v) == 0 {
+				*t = ""
+			}
+			if len(v) > 0 {
+				*t = UUID(v)
+			}
+			return nil
+		}
+	}
+	return errors.New("failed to scan UUID")
+}
+
+// V returns the underlying object
+func (t UUID) V() ulid.ULID {
+	v, _ := ulid.Parse(string(t))
+	return v
+}
+
+// Value implements the database/sql/driver Valuer interface
+func (t UUID) Value() (driver.Value, error) {
+	return string(t), nil
+}
+
+// String implements the fmt Stringer interface
+func (t UUID) String() string {
+	return string(t)
+}
