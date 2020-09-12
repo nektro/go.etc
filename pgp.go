fafa714ff92d3173874bf115d4c8e595c38a6b33
Meghan <hello@nektro.net>
2020-09-12 15:52:43 -0700

    etc: update PgpEncrypt to accpt multiple recipients

--- pgp.go
+++ pgp.go
@@ -12,26 +12,26 @@ import (
 	"golang.org/x/crypto/openpgp/packet"
 )
 
-func PgpEncrypt(c *htp.Controller, keyText, message string) string {
-	block, err := armor.Decode(strings.NewReader(keyText))
-	c.AssertNilErr(err)
-	c.Assert(block.Type == openpgp.PublicKeyType, "403: pgp block must be a public key")
-
-	keyEnt, err := openpgp.ReadEntity(packet.NewReader(block.Body))
-	c.AssertNilErr(err)
+func PgpEncrypt(c *htp.Controller, message string, keys ...string) string {
+	recs := []*openpgp.Entity{}
+	for _, item := range keys {
+		block, err := armor.Decode(strings.NewReader(item))
+		c.AssertNilErr(err)
+		c.Assert(block.Type == openpgp.PublicKeyType, "403: pgp block must be a public key")
+		keyEnt, err := openpgp.ReadEntity(packet.NewReader(block.Body))
+		c.AssertNilErr(err)
+		recs = append(recs, keyEnt)
+	}
 
 	buf := new(bytes.Buffer)
-
 	armWriter, err := armor.Encode(buf, "PGP MESSAGE", map[string]string{})
 	c.AssertNilErr(err)
-
-	pgpWriter, err := openpgp.Encrypt(armWriter, []*openpgp.Entity{keyEnt}, nil, nil, nil)
+	pgpWriter, err := openpgp.Encrypt(armWriter, recs, nil, nil, nil)
 	c.AssertNilErr(err)
 
-	io.Copy(pgpWriter, bytes.NewReader([]byte(message)))
+	io.Copy(pgpWriter, strings.NewReader(message))
 	pgpWriter.Close()
 	armWriter.Close()
-
 	return string(buf.Bytes())
 }
 
